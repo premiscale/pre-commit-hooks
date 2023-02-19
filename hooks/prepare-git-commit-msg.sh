387c744e8b7e29d1feb8a49c165e49a1681a48c9
Emma Doyle <brandon@sbevision.com>
2023-02-19 15:07:18 -0500

    Fix prepare-git-msg hook

--- hooks/prepare-git-commit-msg.sh
+++ hooks/prepare-git-commit-msg.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
+# https://git-scm.com/docs/githooks#_prepare_commit_msg
+
+FILE="$1"
 
 BRANCH_NAME=$(git symbolic-ref --short HEAD)
-BRANCH_NAME="${BRANCH_NAME##*/}"
-BRANCH_IN_COMMIT=$(grep -c "$BRANCH_NAME:" "$1")
 
-if [ -n "$BRANCH_NAME" ] && [ "$BRANCH_EXCLUDED" -ne 1 ] && [ "$BRANCH_IN_COMMIT" -lt 1 ]; then
-    sed -i.bak -e "1s/^/$BRANCH_NAME: /" "$1"
-fi
+sed -i -e "1s/^/$BRANCH_NAME: /" "$FILE"
