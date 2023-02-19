090382647e9b99426ced4c47d971c101f9eb5c44
Emma Doyle <brandon@sbevision.com>
2023-02-19 15:21:30 -0500

    Fix

--- hooks/prepare-git-commit-msg.sh
+++ hooks/prepare-git-commit-msg.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
 # https://git-scm.com/docs/githooks#_prepare_commit_msg
 
-FILE="$1"
+# shellcheck disable=SC2034
+
+COMMIT_MSG_FILE=$1
+COMMIT_SOURCE=$2
+SHA1=$3
 
 BRANCH_NAME=$(git symbolic-ref --short HEAD)
 
-sed -i -e "1s/^/$BRANCH_NAME: /" "$FILE"
+sed -i -e "1s/^/$BRANCH_NAME: /" "$COMMIT_MSG_FILE"
