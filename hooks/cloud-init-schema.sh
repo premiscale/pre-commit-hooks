7335fcdcd9b55cc93961dba4c7502a6cc97cd447
Emma Doyle <emma@sbevision.com>
2023-02-22 12:42:06 -0500

    ENG-140: Add cloud-init schema validation pre-commit hook

---
+++ hooks/cloud-init-schema.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+# Validate a cloud-init schema / user-data file(s).
+
+if ! command -v cloud-init >/dev/null; then
+    printf "ERROR: must install cloud-init to use this hook.\\n" >&2
+    exit 1
+fi
+
+for cfg in "$@"; do
+    cloud-init schema --config-file "$cfg"
+done
