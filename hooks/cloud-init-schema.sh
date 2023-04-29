e8e56627f04f3379922499fabdebe3f8f41eb05a
Emma Doyle <emma@sbevision.com>
2023-04-29 11:06:47 -0400

    master: add gunicorn script config schema validation

--- hooks/cloud-init-schema.sh
+++ hooks/cloud-init-schema.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/env bash
 # Validate a cloud-init schema / user-data file(s).
 
+
 if ! command -v cloud-init >/dev/null; then
     printf "ERROR: must install cloud-init to use this hook.\\n" >&2
+    exit 1
+fi
+
+if [ $# -lt 1 ]; then
+    printf "cloud-init requires at least one argument: config file\\n" >&2
     exit 1
 fi
 
